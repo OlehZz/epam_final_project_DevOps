09b5985de029b0c50d5f96cddec42725bd7f586a
OlehZz <olegzov@bigmir.net>
2021-03-13 23:52:38 +0200

    modify script.sh

--- ansible/script.sh
+++ ansible/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cp /home/ubuntu/jenkins/footgo/src/main/resources/application.properties.example /home/ubuntu/jenkins/footgo/src/main/resources/application.properties
+cp home/ubuntu/jenkins/workspace/'setup jenkins node'/footgo/src/main/resources/application.properties.example home/ubuntu/jenkins/workspace/'setup jenkins node'/footgo/src/main/resources/application.properties
 #export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64"
 export MAVEN_OPTS="-Xmx1024m"
-sed -i 's/spring.datasource.username=root/spring.datasource.username=footgo/g' /home/ubuntu/jenkins/footgo/src/main/resources/application.properties
-sed -i 's/spring.datasource.url=jdbc:mysql:\/\/localhost/spring.datasource.url=jdbc:mysql:\/\/terraform-20200807210322959000000001.cnowmm0u4gtt.us-east-1.rds.amazonaws.com/g' /home/ubuntu/jenkins/footgo/src/main/resources/application.properties
+sed -i 's/spring.datasource.username=root/spring.datasource.username=footgo/g' home/ubuntu/jenkins/workspace/'setup jenkins node'/footgo/src/main/resources/application.properties
+sed -i 's/spring.datasource.url=jdbc:mysql:\/\/localhost/spring.datasource.url=jdbc:mysql:\/\/terraform-20200807210322959000000001.cnowmm0u4gtt.us-east-1.rds.amazonaws.com/g' home/ubuntu/jenkins/workspace/'setup jenkins node'/footgo/src/main/resources/application.properties
