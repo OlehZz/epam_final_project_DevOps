63497c1037689b8ab8fc43b0d50b1002704e6279
OlehZz <olegzov@bigmir.net>
2021-03-18 02:26:23 +0200

    update deploy.sh

--- terraform/modules/instances/deploy.sh
+++ terraform/modules/instances/deploy.sh
@@ -5,6 +5,8 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt install -y docker-ce
 sudo systemctl start docker
 sudo usermod -a -G docker ubuntu
+sudo apt-get install -y python-pip
+sudo pip install docker-py
 sudo apt-get install -y awscli
 sudo $(aws ecr get-login --no-include-email --region us-east-1)
 sudo docker pull 622371100744.dkr.ecr.us-east-1.amazonaws.com/olehzz_footgo
